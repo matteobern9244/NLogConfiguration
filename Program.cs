c66b9dd19ff9b7e251164e1726fee0c76d87e6f0
Matteo Bernardini <matteobern92@gmail.com>
2023-05-13 18:55:57 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+
+namespace NLogConfiguration
+{
+    internal class Program
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Logger.Info("Test");
+        }
+    }
+}
